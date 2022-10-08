e158b767ee093b2a37db1d26929aa069e2404664
aldaine-AG <aldaineclarke1@gmail.com>
2022-10-08 05:26:10 -0500

    created schema for registrant

---
+++ schema/registrant.schema.js
@@ -0,0 +1,37 @@
+const { Schema, model} = require('mongoose')
+let registrantSchema = new Schema({
+	first_name: {
+		type: String,
+		required: [true, 'first name is required'],
+	},
+	last_name: {
+		type: String,
+		required: [true, 'last name is required'],
+	},
+	email_address: { 
+		type: String, 
+		required: [true, 'No email address provided']
+	},
+	phone:{
+		type: String,
+	},
+	department: {
+		type: Schema.Types.ObjectId, ref:"Department", 
+		required: [true, 'A department must be provided for this registration to be valid']}
+
+})
+
+registrantSchema.methods.checkDupe = function () {
+	return new Promise(async (resolve, reject) => {
+		const dupe = await db
+			.model('Registrant')
+			.find({ email_address: this.email_address, department: this.department })
+			.catch((err) => {
+				reject(err)
+			})
+		resolve(dupe.length > 0)
+	})
+}
+
+const registrantModel = model('Registrant', registrantSchema)
+module.exports = registrantModel
