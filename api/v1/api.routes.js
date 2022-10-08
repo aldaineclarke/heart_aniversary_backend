e24d171917360623795e31990305aedb8729d34d
aldaine-AG <aldaineclarke1@gmail.com>
2022-10-08 17:24:00 -0500

    used an html file instead of ejs for api home screen

--- api/v1/api.routes.js
+++ api/v1/api.routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const userRouter =  require("./routes/user.routes");
-
+const path = require("path");
 /**
  * Generates the API Docs from the list of routes in the system and attaches descriptions to them
  * from the descriptions array, when you add routes, it will change on the next load to reflect new routes
@@ -8,49 +8,8 @@ const userRouter =  require("./routes/user.routes");
  * to this order.
  */
  router.all('', (req, res) => {
-	let concat = [];
-    let resource = "";
-    let routerLayers = [...router.stack]
-	for (let routerLayer of routerLayers) {
-        if(!routerLayer.handle.stack) {
-            concat.push({
-                path: routerLayer.route.path,
-                methods: Object.keys(routerLayer.route.methods),
-            })
-            continue
-        };
-        switch(routerLayers.indexOf(routerLayer)){
-            case 1: resource = "users";break;
-            case 2: resource = "departments";break;
-            case 3: resource = "registrants";break;
-            case 4: resource = "authenticate";break;
-        }
-        for(let layer of routerLayer.handle.stack){
-            concat.push({
-                path: resource+layer.route.path,
-                methods: Object.keys(layer.route.methods),
-            })
-        }
-		
-    }
-
-	const descriptions = [
-		`API DOCS URL`,
-		`Route for creating user and getting all users in the database`,
-		`Route for updating, deleting and retrieving a single user`,
-		`Gets all departments and allow for creation of departments`,
-		`Retrieving, updating and deleting a single department`,
-		`Creating and retrieving all registrants `,
-		`Update, retrieve and delete a single registrant`,
-		`Authenticate user  and creates token`,
-	]
-	let body = {
-		name: 'Heart Registration App v1',
-		version: '1.0.0',
-		routes: concat,
-		description: descriptions,
-	}
-	res.render('summary', body)
+	
+	res.sendFile(path.resolve(__dirname,"../../index.html"));
 });
 
 router.use("/users",userRouter);
