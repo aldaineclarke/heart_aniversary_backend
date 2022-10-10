0b1f86a2200f86ccc2e11934e2c86a2b7c80d679
aldaine-AG <aldaineclarke1@gmail.com>
2022-10-10 07:02:22 -0500

    updated nodemailer to accept attachements

--- utilities/nodemailer.js
+++ utilities/nodemailer.js
@@ -20,12 +20,20 @@ class Emailer {
 	 * @param {*} sub - The subject of the email
 	 * @param {*} body - The body of the email
 	 */
-	sendMail(to, sub, body) {
+	sendMail(to, sub, body, attachment) {
+		let selectedAttachment = [];
+		// check if attachment is an array;
+		if(Array.isArray(attachment)){
+			selectedAttachment = attachment;
+		}else{
+			selectedAttachment.push(attachment);
+		}
 		let mailOptions = {
 			to: to,
 			from: UMAIL,
 			subject: sub,
 			text: body,
+			attachments:selectedAttachment,
 		}
 		this.#transporter.sendMail(mailOptions, function (error, info) {
 			if (error) {
