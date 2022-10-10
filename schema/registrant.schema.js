e0c34baaa8df24daf92e5a6f14cec66a2d53df26
aldaine-AG <aldaineclarke1@gmail.com>
2022-10-10 13:12:25 -0500

    Removed has participated property from the registrant schema

--- schema/registrant.schema.js
+++ schema/registrant.schema.js
@@ -31,14 +31,12 @@ let registrantSchema = new Schema({
 		required: [true, 'A department must be provided for this registration to be valid']
 	},
 	organization:{type: String, required: [true, 'A organization must be provided']},
-	hasParticipated: {type: Boolean, default: false},
 
 })
 
 
 registrantSchema.pre("save", async function(next){
 	let verbage = await Verbage.findOne({"name": "WELCOME_EMAIL"});
-	console.log(verbage);
 	let data = {
 		registrant :this.first_name + " " +this.last_name,
 		demonstrator : "Peter Parker"
@@ -55,63 +53,12 @@ registrantSchema.pre("save", async function(next){
 				  path: path.resolve(__dirname,"../assets/PARTICIPANT_CERTIFICATE.pdf")
 		}
 		);
-
-	// let department = await Department.findById(this.department);
-	// if(!department) return Promise.reject(new Error("Invalid department ID"))
-	// let cert = await pdfMaker.GenerateCertificatePDF({
-	//    user: `${this.first_name} ${this.last_name}`,
-	//    department: department.name
-	//  })
-	// let mailData = {
-	//    recipient: this.email_address,
-	//    subject:`${department.name} Booth Registration`,
-	//    html:`Thank you for participating at the ${department.name} booth,\n
-	// 		 please see attached, your certificate showing the new skill that you have learnt`,
-	//    attachments:[{
-	// 	  content: cert,
-	// 	  filename: "Participation_Certificate.pdf",
-	// 	  type: "application/pdf",
-	// 	  disposition: "attachment"
-	//    }]
-	// }
-	// await sendMail(mailData);
-	// pdfMaker.deleteGeneratedPDF("Participation_Certificate.pdf");
 })
 
 registrantSchema.methods.assignRegNum = async function (){
 	console.log("Assign number")
 	return await generateRegNumber("13", 8);
 }
-registrantSchema.pre("findOneAndUpdate", async function(next){
-	let doc = this.model.findOne(this.getQuery());
-	// if the update is to change participated to true then it will send email.
-	if(this._update.hasParticipated){
-		mailer.sendMail(this.email_address, "Email Test with SMTP Server", "This is a test to confirm that the server is working");
-
-		if(!doc) return Promise.reject(new Error("No user found to update"));
-		let department = await Department.findById(doc.department);
-		if(!department) return Promise.reject(new Error("Invalid department ID"))
-		let cert = await pdfMaker.GenerateCertificatePDF({
-		   user: `${doc.first_name} ${doc.last_name}`,
-		   department: department.name
-		 })
-		let mailData = {
-		   recipient: doc.email_address,
-		   subject:`${department.name} Booth Registration`,
-		   html:`Thank you for participating at the ${department.name} booth,\n
-				 please see attached, your certificate showing the new skill that you have learnt`,
-		   attachments:[{
-			  content: cert,
-			  filename: "Participation_Certificate.pdf",
-			  type: "application/pdf",
-			  disposition: "attachment"
-		   }]
-		}
-		await sendMail(mailData);
-		pdfMaker.deleteGeneratedPDF("Participation_Certificate.pdf");
-	}
-	
-})
 
 registrantSchema.methods.checkDupe = function () {
 	return new Promise(async (resolve, reject) => {
